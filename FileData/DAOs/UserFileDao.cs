290fffd6edee682d0a240497931b39c87953a419
lKrollo <rolf04@live.dk>
2023-09-24 11:04:56 +0200

    f

--- FileData/DAOs/UserFileDao.cs
+++ FileData/DAOs/UserFileDao.cs
@@ -1,6 +1,26 @@
+using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Domain.Models;
+
 namespace FileData.DAOs;
 
-public class UserFileDao
+public class UserFileDao : IUserDao
 {
-    
+    private readonly FileContext _context;
+
+    public UserFileDao(FileContext context)
+    {
+        _context = context;
+    }
+
+
+    public Task<User> CreateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByUsernameAsync(string Username)
+    {
+        throw new NotImplementedException();
+    }
 }
